e9da0ebead74d4862d9839147db5a72fef00d48e
Avdhutjarag <103016757+Avdhutjarag@users.noreply.github.com>
2024-01-03 22:00:59 +0530

    Update variables.groovy

--- variables.groovy
+++ variables.groovy
@@ -1,7 +1,7 @@
 // variables.groovy
+
 return [
-    variable1: 'some value',
-    variable2: 42,
-    variable3: true,
-    variable4: ['item1', 'item2', 'item3']
+    variable1: 'value1',
+    variable2: 'value2',
+    variable3: 'value3'
 ]
